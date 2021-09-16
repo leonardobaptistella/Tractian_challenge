fe9befed4cf4d4b9f2886ebb8803f174bd8e057b
Leonardo Baptistella <leonardo.ribeirobaptistella@usp.br>
2021-09-16 19:24:29 -0300

    parse the file name

--- import_data_function.cpp
+++ import_data_function.cpp
@@ -4,32 +4,19 @@
 #include <sstream> //Parse the file
 #include <vector>
 #include <fstream> //Read and Write Files
-
 using namespace std; 
 
-int main() {
 
-	// define variables
-	string X, Y, Z  ;
+vector<float> import_data(string file){
+    // define variables
+	string X, Y, Z  ; //variables from file are here
 	vector<float>X_V;
 	vector<float>Y_V;
 	vector<float>Z_V;
-	
-	//input filename
-	string file;
-	file = "01-03-S23.txt";
-	
-	//parse file name
-	std::istringstream split(file);
-	string inicio, duracao, id_sensor; 
-	getline(split, inicio, '-');
-	getline(split, duracao, '-');
-    getline(split, id_sensor, '.');
-
+	    
 	//number of lines
 	int i = 0;
 
-	// File lines to Array
 	ifstream coeff(file); //opening the file.
 	if (coeff.is_open()) //if the file is open
 	{
@@ -37,21 +24,21 @@ int main() {
 		string line;
 		getline(coeff, line);
 
-		while (!coeff.eof()) 
+		while (!coeff.eof()) //while the end of file is NOT reached
 		{
 			getline(coeff, X, ',');
 			X_V.push_back(stof(X));
 			getline(coeff, Y, ',');
 			Y_V.push_back(stof(Y));
-			getline(coeff, Z, '\n'); 
+			getline(coeff, Z, '\n'); //new line after Z
 			Z_V.push_back(stof(Z));
 			
-			i += 1; 
+			i += 1; //increment number of lines
 		}
 		coeff.close(); //closing the file
-		//cout << "Number of entries: " << i-1 << endl;
+		cout << "Number of entries: " << i-1 << endl;
 	}
-	else cout << "Unable to open file"; 
+	else cout << "Unable to open file"; //if the file is not open output
 	
 	/*for(int j=0; j<=i-1;j++){
 		cout << X_V[j];
@@ -60,15 +47,14 @@ int main() {
 		cout << "\t";
 		cout << Z_V[j];
 		cout << "\n";
-	}*/
-	
-	cout << inicio; 
-	cout << "\n";
-	cout << duracao;
-	cout << "\n";
-	cout << id_sensor;
-	cout << "\n";	
+	} */
+   
+    return X_V , Y_V, Z_V;
+}
 
+int main() {
+    string file_name = "sensor_data.txt";
+	import_data(file_name);
 	return 0;
 
 }
